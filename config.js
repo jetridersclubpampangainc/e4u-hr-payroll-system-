466efda8fdd213f4c4278c4e1b36bba66bc5d7d6
jetridersclubpampangainc <293566750+jetridersclubpampangainc@users.noreply.github.com>
2026-07-20 12:59:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.E4U_SUPABASE_CONFIG = {
-  url: "https://ipfgmsefgaglwofiunzf.supabase.co",
-  anonKey:https://ipfgmsefgaglwofiunzf.supabase.co/rest/v1/
+url: "https://ipfgmsefgaglwofiunzf.supabase.co",",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwZmdtc2VmZ2FnbHdvZml1bnpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMDMxNDUsImV4cCI6MjA5ODg3OTE0NX0.bsO5fjUGbqjpVePiOhhie9pX7UyYLo8r_TsQ5Ztpqo4"
 };
